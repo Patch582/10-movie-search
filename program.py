5051bf6589cde959f1575b2af2562aad482efcbd
Pat Booth <patrickbooth582@gmail.com>
2019-04-21 11:39:29 +1000

    Refactored search, search results and output

--- program.py
+++ program.py
@@ -18,7 +18,7 @@ def main():
     print_header()
 
     # TODO: grab search key & set URL
-    search = 'capital'
+    search = input('What movie do you want to search for? ')
     url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search)
 
     # TODO: grab movie data
@@ -29,9 +29,28 @@ def main():
     # TODO: display movie data
 
     movie_data = resp.json()
-    movies = movie_data.get('hits')
+    movies_list = movie_data.get('hits')
 
-    print(type(movies), movies)
+    # print(type(movies_list), movies_list)
+
+    movies = []
+    for md in movies_list:
+        m = MovieResult(
+            imdb_code=md.get('imdb_code'),
+            title=md.get('title'),
+            duration=md.get('duration'),
+            director=md.get('director'),
+            year=md.get('year', 0),
+            rating=md.get('rating', 0),
+            imdb_score=md.get('imdb_score', 0.0),
+            keywords=md.get('keywords'),
+            genre=md.get('genre')
+        )
+        movies.append(m)
+
+    print("Found {} movies searching for '{}'".format(len(movies), search))
+    for m in movies:
+        print("{} -- {}".format(m.year, m.title))
 
 if __name__ == '__main__':
     main()
